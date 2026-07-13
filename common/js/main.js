ad9593d8803bde2e6eff5c19c2b52846eedffca1
trwlyn <r1012723@student.thomasmore.be>
2026-07-14 00:36:14 +0200

    modified main.js

--- common/js/main.js
+++ common/js/main.js
@@ -108,7 +108,7 @@ if (contactForm) {
         };
 
         try {
-            const response = await fetch("http://127.0.0.1:8000/api/contact", {
+            const response = await fetch("https://herkansing-backend.onrender.com/api/contact", {
                 method: "POST",
                 headers: { "Content-Type": "application/json" },
                 body: JSON.stringify(formData)
